540c7af6502cf2c52bf94d2b0dd23af9e85cd990
aminikhil <waykosn@gmail.com>
2019-12-27 14:19:08 +0530

    Services updated with css

--- js/js1.js
+++ js/js1.js
@@ -8,3 +8,17 @@ function scrollFunction() {
     document.getElementById("navbar").style.backgroundColor = "rgb(255,255,255,0.3)";
   }
 }
+
+function expandCardView() {
+	var style = document.getElementById("cardView1").style.display;
+	// alert(style);
+	if(style == ""){
+		document.getElementById("cardView1").style.display="block";
+		document.getElementById("cardView1on").style.display = "none";
+		document.getElementById("cardView1off").style.display="block";
+	} else {
+		document.getElementById("cardView1").style.display="";
+		document.getElementById("cardView1off").style.display="";
+		document.getElementById("cardView1on").style.display="block";
+	}	
+}
